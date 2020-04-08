dd7df62b14d3b16b85fe89cef9fd7083e07df18d
mazation <mazation96@gmail.com>
2020-04-09 02:44:44 +0500

    First Commit

---
+++ Counter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Counter
+    {
+        private int countValue;
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Value
+        {
+            get { return countValue; }
+            set
+            {
+                if (value < Min || value > Max)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    countValue = value;
+                }
+            }
+        }
+
+        public Counter() {
+            Min = 0;
+            Max = 0xffff;
+            countValue = 0;
+        }
+        
+        public Counter(int minimum, int maximum) {
+            Min = minimum;
+            Max = maximum;
+            countValue = minimum;
+        }
+
+        public Counter(int minimum, int maximum, int start) {
+            Min = minimum;
+            Max = maximum;
+            countValue = start;
+        }
+
+        public static Counter operator ++ (Counter p1) {
+            return new Counter(p1.Min, p1.Max, p1.Value++);
+        }
+
+        public static Counter operator -- (Counter p1)
+        {
+            return new Counter(p1.Min, p1.Max, p1.Value--);
+        }
+
+        public string currentState() {
+            return "Счетчик сейчас в значении " + Convert.ToString(Value, 16);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter(1, 15, 0xB);
+            Counter c2 = c++;
+            Counter c3 = c--;
+            Console.WriteLine(c.Value);
+            Console.WriteLine(c2.Value);
+            Console.WriteLine(c3.Value);
+            Console.WriteLine(c3.currentState());
+            Console.ReadLine();
+        }
+    }
+}
